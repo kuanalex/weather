eb5a804ecc86a773d337a94e4eefdf47b8046432
kuanalex <59577267+kuanalex@users.noreply.github.com>
2021-06-05 10:55:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 async function getWeather() {
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Markham, CA&appid=08a911be2a5e6a05de2068aeb4643b3b&units=metric', {
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Markham, CA&appid=YOUR_APP_ID&units=metric', {
         mode: 'cors'
     });
     const weatherData = await response.json();
